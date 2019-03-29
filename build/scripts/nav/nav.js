15cb43ea7f8c7a8de1f940e18b9ba6d5591e9404
burtart <bburtart@gmail.com>
2019-03-29 09:02:04 +0200

    commit

--- build/scripts/nav/nav.js
+++ build/scripts/nav/nav.js
@@ -5,3 +5,34 @@ $('#bar-toggle').click(function(){
 $('.mus-btn').click(function(){
     $('.mus-btn').toggleClass("mus-btn__play mus-btn__stop");
 })
+var musBtn = document.querySelector(".mus-btn");
+var audio = document.getElementById("bgAudio");
+var timer;
+audio.currentTime = sessionStorage.getItem("currentTime");
+if (sessionStorage.getItem("status") == "play") {
+	musBtn.classList = "mus-btn mus-btn__stop";
+	audio.play();
+	timer = setInterval(function () {
+		sessionStorage.setItem("currentTime", audio.currentTime), 250
+	})
+}
+
+musBtn.onclick = function () {
+	if (musBtn.classList.contains("mus-btn__stop")) {
+		audio.play();
+		timer = setInterval(function () {
+			sessionStorage.setItem("currentTime", audio.currentTime)
+		}, 250)
+		sessionStorage.setItem("status", "play")
+
+	}
+	if (musBtn.classList.contains("mus-btn__play")) {
+		audio.pause();
+		sessionStorage.setItem("currentTime", audio.currentTime)
+		clearInterval(timer);
+		sessionStorage.setItem("status", "stop")
+
+
+	}
+
+}
